838b09c14f064e56503d1d983da515d8e7e5f0c3
Andrey Lebedev <alebedev424@gmail.com>
2023-11-07 16:12:01 +0300

    feater: доделал секцию gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -15,15 +15,28 @@ function Gallery() {
       <p className='gallery__sub sub'>We focus on ergonomics and meeting you where you work. It's only a keystroke away.</p>
       <ul className='gallery__cards list'>
         <li>
-          <img className='gallery__imag' alt='Rectangle_1' src={Rectangle_1} />
+          <img className='gallery__imag' alt='Rectangle_1' src={Rectangle_1}/>
+        </li>
+        <li>
           <img className='gallery__imag' alt='Rectangle_2' src={Rectangle_2}/>
+        </li>
+        <li>
           <img className='gallery__imag' alt='Rectangle_3' src={Rectangle_3}/>
+        </li>
+        <li>
           <img className='gallery__imag' alt='Rectangle_4' src={Rectangle_4}/>
+        </li>
+        <li>
           <img className='gallery__imag' alt='Rectangle_5' src={Rectangle_5}/>
+        </li>
+        <li>
           <img className='gallery__imag' alt='Rectangle_6' src={Rectangle_6}/>
+        </li>
+        <li>
           <img className='gallery__imag' alt='Rectangle_7' src={Rectangle_7}/>
         </li>
       </ul>
+      <button className='gallery__button button'>See more</button>
     </section >
   )
 }
