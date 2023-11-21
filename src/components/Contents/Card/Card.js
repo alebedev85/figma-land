f03670ab850aded8dfb56f7fb0ec3390742a5c75
Andrey Lebedev <alebedev424@gmail.com>
2023-11-21 14:43:25 +0300

    refactor: переделал Card.scss на module.scss

--- src/components/Contents/Card/Card.js
+++ src/components/Contents/Card/Card.js
@@ -1,15 +1,15 @@
-import './Card.scss';
+import styles from'./Card.module.scss';
 
 function Card({headline, sub, button_text, img, gap}) {
 
   return (
-    <section className='card' aria-label='card' style={{ gap: gap}}>
-      <div className='card_text'>
-        <h3 className='card__headline'>{headline}</h3>
-      <p className='card__sub'>{sub}</p>
+    <section className={styles.card} aria-label='card' style={{ gap: gap}}>
+      <div className={styles.text}>
+        <h3 className={styles.headline}>{headline}</h3>
+      <p className={styles.sub}>{sub}</p>
       </div>
-      <button className='card__button button'>{button_text}</button>
-      <img className='card__image' src={img}></img>
+      <button className={`${styles.button} button`}>{button_text}</button>
+      <img className={styles.image} src={img}></img>
     </section >
   )
 }
