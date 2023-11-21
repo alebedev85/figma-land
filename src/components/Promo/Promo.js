8995d72aaf192c3d280163d66ee51965ecb36329
Andrey Lebedev <alebedev424@gmail.com>
2023-11-21 14:04:23 +0300

    refactor: переделал Promo.scss на module.scss

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,23 +1,23 @@
-import './Promo.scss';
+import styles from './Promo.module.scss';
 import screens from '../../images/screens.svg'
 
 function Promo() {
 
   return (
-    <section className='promo' aria-label='Project presentation'>
-      <h2 className='promo__headline'>Work at the speed of thought</h2>
-      <p className='promo__sub'>Most calendars are designed for teams.
+    <section className={styles.promo} aria-label='Project presentation'>
+      <h2 className={styles.headline}>Work at the speed of thought</h2>
+      <p className={styles.sub}>Most calendars are designed for teams.
         Slate is designed for freelancers who want a simple way to plan their schedule.</p>
-      <div className='promo__buttons'>
-        <button className='promo__button_try button'>
-          <p className='promo__button__text'>Try For Free</p>
+      <div className={styles.buttons}>
+        <button className={`button ${styles.button_try}`}>
+          <p className={styles.button__text}>Try For Free</p>
         </button>
-        <button className='promo__button_learn button'>
-          <p className='promo__button__text'>Learn More</p>
+        <button className={`button ${styles.button_learn}`}>
+          <p className={styles.button__text}>Learn More</p>
         </button>
       </div>
-      <div className='promo__container'>
-        <img className='promo__screens' src={screens} alt='Presentation of Screens' />
+      <div className={styles.container}>
+        <img className={styles.screens} src={screens} alt='Presentation of Screens' />
       </div>
     </section >
   )
