61ffe8626740aa9bf303cafa549a4121ed7c6586
Andrey Lebedev <alebedev424@gmail.com>
2024-11-12 11:44:36 +0300

    feat: сделал выпадающее меню, сделал адаптив в header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,20 @@
-import styles from "./Header.module.scss";
-import logo from "../../images/logo.svg";
+import cn from "classnames";
+import Logo from "../../images/logo.svg";
+import { ReactComponent as MenuButton } from "../../images/menu-button.svg";
 import Button from "../ui/Button/Button";
+import { useState } from "react";
+
+import styles from "./Header.module.scss";
 
 function Header() {
+  const [isOpen, setOpen] = useState(false);
+  console.log(isOpen)
   return (
     <header className={styles.header}>
       <a className="link" href="#">
-        <img src={logo} className={styles.logo} alt="Логотип сайта" />
+        <img src={Logo} className={styles.logo} alt="Логотип сайта" />
       </a>
-      <ul className={`list ${styles.navbar}`}>
+      <ul className={`list ${styles.navbar} ${isOpen ? styles.navbar_active : ""}`}>
         <li>
           <a className={`link ${styles.link}`} href="#">
             Home
@@ -30,7 +36,12 @@ function Header() {
           </a>
         </li>
       </ul>
-      <Button variant="secondary" text="Login"/>
+      <div className={styles.buttons} >
+        <Button variant="secondary" text="Login" />
+        <button className={styles.menuButton} onClick={() => setOpen(!isOpen)}>
+          <MenuButton />
+        </button>
+      </div>
     </header>
   );
 }
